e56ec08f2f43f63eb7e9ce6c48cf46af0db34b6b
Romil003 <romilghadge33@gmail.com>
2022-11-04 22:02:56 +0530

    Added Mobile Number in User Registration UC4

--- com/bridgelabz/UserRegistration.java
+++ com/bridgelabz/UserRegistration.java
@@ -12,6 +12,8 @@ public class UserRegistration {
 
     static Pattern emailId = Pattern.compile("^([a-z\\.]+)([a-z]*)@([a-z]{2,}+)\\.([a-z\\.]{2,}+)([a-z]*)$");
 
+    static Pattern mobileNumber = Pattern.compile("^([0-9]{2,4}+)\\ ([0-9]{10}+)$");
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -35,9 +37,19 @@ public class UserRegistration {
         }
 
         System.out.println();
-        System.out.println("Enter Last name : ");
+        System.out.println("Enter Email ID : ");
         Matcher matcher3 = emailId.matcher(input.next());
         if(matcher3.matches()){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+
+        System.out.println();
+        System.out.println("Enter Mobile number With country code : ");
+        Matcher matcher4 = mobileNumber.matcher(input.next());
+        if(matcher4.matches()){
             System.out.println(true);
         }
         else {
